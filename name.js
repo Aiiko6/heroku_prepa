cda489d9da9b8ac207c2fa80bd4bee54512da6b7
AsadaJason <monkeydieluffy5@gmail.com>
2017-04-28 21:30:54 +0200

    Update name.js

--- name.js
+++ name.js
@@ -1,1 +1,26 @@
+const discord = require('discord.js');
+const client = new discord.Client();
 
+
+
+client.on("ready", () => {
+  var servers = client.guilds.array().map(g => g.name).join(',');
+  console.log("--------------------------------------");
+  console.log('[!]Connexion en cours... \n[!]Veuillez Patienté! \n[!]Les évenement sont après ! :)  \n[!]Les préfix actuelle:  ' + prefix + "\n[!]Mentions = " + mention);
+  client.user.setGame("#test", "http://twitch.tv/highwaytraffic"); //Joue à ...
+});
+
+client.on('message', message =>{
+  if(message.content.startsWith('#test'){
+      message.reply('yes');
+     }
+});
+
+//identification
+var dt = process.env.DISCORD_TOKEN || process.argv[2];
+
+if (!dt) {
+    console.log('votretoken');
+}
+
+client.login(dt);
